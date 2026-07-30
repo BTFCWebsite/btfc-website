09a88f8893882e2c74a16405b481b557fd18246e
BTFCWebsite <info@brimscombeandthruppfc.co.uk>
2026-07-30 13:43:04 +0300

    Enable sponsor profile links

--- app/template.tsx
+++ app/template.tsx
@@ -1,11 +1,13 @@
 import type { ReactNode } from 'react'
 import HomeNextFixture from './HomeNextFixture'
+import SponsorTileLinks from './SponsorTileLinks'
 
 export default function Template({ children }: { children: ReactNode }) {
   return (
     <>
       {children}
       <HomeNextFixture />
+      <SponsorTileLinks />
     </>
   )
 }
