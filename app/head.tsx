9e39b58432342ddf5942ac4c69847b27c8899eb1
BTFCWebsite <info@brimscombeandthruppfc.co.uk>
2026-08-08 10:29:05 +0300

    Preload homepage hero image and crest

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="preload" as="image" href="/matchday/Ground_Pic.jpeg" />
+      <link rel="preload" as="image" href="/branding/crest.png" />
+    </>
+  )
+}
