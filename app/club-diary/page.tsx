d932735fb492873ddd6dec001d22c91034a6b03a
BTFCWebsite <info@brimscombeandthruppfc.co.uk>
2026-08-24 10:03:39 +0100

    Wire reliable month tile navigation

--- app/club-diary/page.tsx
+++ app/club-diary/page.tsx
@@ -3,6 +3,7 @@ import ClubDiaryPreGate from './ClubDiaryPreGate'
 import ClubDiarySecurityTools from './ClubDiarySecurityTools'
 import ClubDiaryLogoutSync from './ClubDiaryLogoutSync'
 import ClubDiaryMonthDetails from './ClubDiaryMonthDetails'
+import ClubDiaryMonthTileNavigation from './ClubDiaryMonthTileNavigation'
 import ClubDiaryWorkingPartyShare from './ClubDiaryWorkingPartyShare'
 import ClubDiaryDeleteFeedback from './ClubDiaryDeleteFeedback'
 import ClubDiaryHeroControls from './ClubDiaryHeroControls'
@@ -20,6 +21,7 @@ export default function ClubDiaryPage() {
     <ClubDiarySecurityTools />
     <ClubDiaryLogoutSync />
     <ClubDiaryMonthDetails />
+    <ClubDiaryMonthTileNavigation />
     <ClubDiaryWorkingPartyShare />
     <ClubDiaryDeleteFeedback />
     <ClubDiaryHeroControls />
